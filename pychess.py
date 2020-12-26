c5008d780d3f1f44576694482b116f49e6ff9a44
Sean Shields <sean.shields@tcgplayer.com>
2020-12-26 15:27:24 -0500

    try to caclulate square

--- pychess.py
+++ pychess.py
@@ -3,7 +3,9 @@ import chess
 import chess.svg
 import time
 import threading
+import math
 from random import randint
+from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
 from weights import Weights
@@ -16,28 +18,56 @@ class MoveProbability:
 class AutoChess(QWidget):
   def __init__(self):
     super().__init__()
-    widgetHeightAndWidth = 800
-    boardHeightAndWidth = widgetHeightAndWidth - 30
-
+    self.XSquares = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    self.YSquares = [1, 2, 3, 4, 5, 6, 7, 8]
+    self.widgetHeightAndWidth = 800
+    self.padding = 30
+    self.squareHeightAndWidth = (self.widgetHeightAndWidth - self.padding) / 8
     self.board = chess.Board()
     self.setWindowTitle('Auto Chess')
-    self.setGeometry(100, 100, widgetHeightAndWidth, widgetHeightAndWidth)
     self.widgetSvg = QSvgWidget(parent=self)
-    self.widgetSvg.setGeometry(10, 10, boardHeightAndWidth, boardHeightAndWidth)
+    self.widgetSvg.setGeometry(0, 0, self.widgetHeightAndWidth, self.widgetHeightAndWidth)
     self.widgetSvg.load(chess.svg.board(self.board).encode('UTF-8'))
     self.show()
-  
+
+  @pyqtSlot(QWidget)
+  def mousePressEvent(self, event):
+    print('clicked on: ' + self.getXSquare(event.x()))
+
+  @pyqtSlot(QWidget)
+  def mouseReleaseEvent(self, event):
+    if self.clickIsOnBoard(event.x(), event.y()):
+      print('released on board')
+
+  def clickIsOnBoard(self, x, y):
+    return (x > self.padding
+      and x < self.widgetHeightAndWidth - self.padding
+      and y > self.padding
+      and y < self.widgetHeightAndWidth - self.padding)
+
+  def getXSquare(self, x):
+    squareStart = self.padding
+    for xSquare in self.XSquares:
+      squareEnd = squareStart + self.squareHeightAndWidth
+      if x >= squareStart and x <= squareEnd:
+        print(str(x))
+        print('square ' + xSquare + ' start: ' + str(squareStart) + ' end: ' + str(squareEnd))
+        print('gap: ' + str(squareEnd - squareStart))
+        return xSquare
+      squareStart = squareEnd
+    return 'None'
+
   def refresh(self):
     self.widgetSvg.load(chess.svg.board(self.board).encode('UTF-8'))
 
-  def start(self):
+  def auto(self):
     while not self.board.is_game_over():
       self.calculateNextMove()
     else:
       print('Result: ' + self.board.result())
       self.board.reset()
       self.refresh()
-      self.start()
+      self.auto()
   
   def currentColor(self):
     return 'White' if self.board.turn == chess.WHITE else 'Black'
@@ -87,6 +117,6 @@ class AutoChess(QWidget):
 if __name__ == '__main__':
   app = QApplication(sys.argv)
   autoChess = AutoChess()
-  gameThread = threading.Thread(target=autoChess.start)
+  gameThread = threading.Thread(target=autoChess.auto)
   gameThread.start()
   app.exec_()
